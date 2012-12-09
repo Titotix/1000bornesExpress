fd5cdf04b9e2f756fa8ce41e9f9d990d9c86dd88
Damien MOUCHET <dam.mouchet@gmail.com>
2012-12-09 18:59:10 +0100

    c'est bon la ?

---
+++ src/modele/Accident.java
@@ -0,0 +1,14 @@
+package modele;
+
+public class Accident extends Attaque {
+
+	public Accident() {
+		
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String toString() {
+		return "Accident";
+	}
+
+}
